e7e154d112e6055e411523a9fa18d6610d97697a
Giovani Dardani <gdardani@gdardani-Latitude-E4310.(none)>
2012-07-19 16:25:27 -0300

    Indentation adjustment

--- p4wd/hbridge.ino
+++ p4wd/hbridge.ino
@@ -1,65 +1,118 @@
 // Author: Giovani <giovanidardani at gmail.com>
 // Date: 14/07/2012
 
-#include "p4wd.h"
-#include "utils.h"
 #include "hbridge.h"
 
-#define EN1 8
-#define EN2 9
-#define EN3 12
-#define EN4 13
-#define ENA 10
-#define ENB 11
-
-#define SPEED 130
-
-Hbridge hbridge(EN1, EN2, EN3, EN4, ENA, ENB);
-
-void Run_Cmd(int cmd) {
-  
-  switch (cmd){
-
-    //foward (w)
-    case 119:
-     hbridge.forward(SPEED,SPEED);
-     break;
-    
-    //backward (s) 
-    case 115:
-     hbridge.backward(SPEED,SPEED);
-     break;
-     
-    //left (a)
-    case 97:
-     hbridge.left(SPEED,SPEED);
-     break;
-    
-    //right (d) 
-    case 100:
-     hbridge.right(SPEED,SPEED);
-     break;
-     
-    //stop (q) 
-    case 113:
-     hbridge.stop();
-     break;
-  };
-};
-  
-  
-void setup()
+Hbridge::Hbridge(
+  int _en1,
+  int _en2,
+  int _en3,
+  int _en4,
+  int _ena,
+  int _enb)
 {
-  Serial.begin(9600);
-  Serial.print("ON --> Running at speed: ");
-  Serial.println(SPEED);
-  hbridge.stop();
+  last_dir = 0;
+  en1 = _en1;
+  en2 = _en2;
+  en3 = _en3;
+  en4 = _en4;
+  ena = _ena;
+  enb = _enb;
+
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(en3, OUTPUT);
+  pinMode(en4, OUTPUT);
+  pinMode(ena, OUTPUT);
+  pinMode(enb, OUTPUT);
 }
 
-void loop()
-{ 
-  int cmd = serial_read();
-  Run_Cmd(cmd);
-  //delay(100);
+// move forward
+void Hbridge::forward(int a, int b)
+{
+  if(last_dir == DFORWARD)
+    return;
+
+  last_dir = DFORWARD;
+  back_forward = DFORWARD;
+
+  digitalWrite(en1, LOW);
+  digitalWrite(en2, HIGH);
+
+  digitalWrite(en3, HIGH);
+  digitalWrite(en4, LOW);
+
+  speed(a, b);
 }
 
+// move backward
+void Hbridge::backward(int a, int b)
+{
+  if(last_dir == DBACKWARD)
+    return;
+
+  last_dir = DBACKWARD;
+  back_forward = DBACKWARD;
+
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, LOW);
+
+  digitalWrite(en3, LOW);
+  digitalWrite(en4, HIGH);
+
+  speed(a, b);
+}
+// turn left
+void Hbridge::left(int a, int b)
+{
+  
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, LOW);
+  
+  digitalWrite(en3, HIGH);
+  digitalWrite(en4, LOW);
+  
+  speed(a, b);
+  
+  last_dir = DLEFT;
+}
+
+// turn right
+void Hbridge::right(int a, int b)
+{
+    digitalWrite(en1, LOW);
+    digitalWrite(en2, HIGH);
+
+    digitalWrite(en3, LOW);
+    digitalWrite(en4, HIGH);
+    
+    speed(a, b);
+    //return;
+    
+  last_dir = DLEFT;
+}
+
+// stop motors
+void Hbridge::stop()
+{
+  last_dir = DSTOP;
+
+  digitalWrite(en1, LOW);
+  digitalWrite(en2, LOW);
+
+  digitalWrite(en3, LOW);
+  digitalWrite(en4, LOW);
+
+  analogWrite(ena, 0);
+  analogWrite(enb, 0);
+}
+
+// speed direction of motors
+boolean Hbridge::speed(int a, int b) {
+
+  analogWrite(ena, a);
+  analogWrite(enb, b);
+
+  return true;
+}
+ 
