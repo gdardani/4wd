dd82d17df56a59b02965c035b95870bece5fc2a4
Giovani Dardani <gdardani@gdardani-Latitude-E4310.(none)>
2012-11-11 17:03:04 -0200

    Alpha GUI - version 0.1

--- gui/gui.py
+++ gui/gui.py
@@ -5,55 +5,86 @@
 ## Python 2.7.3
 ## Version: 0.1
 
-from Tkinter import *
+import pygtk
+pygtk.require('2.0')
+import gtk
 import serial
+import time
+from optparse import OptionParser
+
+########################################################
+## cli parser
+########################################################
+
+usage = (
+        "Usage: %prog --d <device> -s <speed>"
+)
+
+cparser = OptionParser(usage=usage, version="%prog 0.5")
+cparser.add_option("--d", "--device", action="store", type="str", dest="device", help="Serial device - ex: /dev/ttyUSB0", default='/dev/ttyUSB0')
+cparser.add_option("--s", "--speed", action="store", type="str", dest="speed", help="Serial Speed ex: 9600", default='9600')
+opts, args = cparser.parse_args()
 
 ## Serial port
-serPort = '/dev/null'
-#serPort = '/dev/ttyUSB0'
-rateLimit = '9600'
-timeOut = 3
+serPort = opts.device
+rateLimit = opts.speed
+timeOut = '3'
 
 ########################################################
 ## Serial Conn
 ########################################################
 
-class SerialConn():
+class sendCmd:
+
  def __init__(self):
- #def __init__(self,serPort, rateLimit, timeOut):
-  self.serPort=serPort
-  self.rateLimit=rateLimit
-  self.timeOut=timeOut
   try:
-   self.bot0 = serial.Serial(self.serPort, self.rateLimit, self.timeOut)
+   self.bot = serial.Serial(serPort, rateLimit)
+   #self.bot = serial.Serial(serPort, rateLimit, timeOut)
    time.sleep(3);
+   
   except Exception as err:
    print "Falha conn porta serial: {0}".format(err)
-   exit (1)
 
-class Application(Frame):
- def sendCommando(self):
-  print "hi there, everyone!"
+ def fwd(self, bla, bla1):
+  try:
+   self.bot.write('w')
+  except Exception as err:
+   print "Falha enviar comando: {0}".format(err)
 
- def createWidgets(self):
-  self.conn = SerialConn()
-  self.left = Button(self)
-  self.left["text"] = '<--'
-  self.left["fg"]   = "blue"
-  self.left["command"] =  self.quit
-  self.left.pack({"side": "left"})
+ def stop(self, bla, bla1):
+  try:
+   self.bot.write('q')
+  except Exception as err:
+   print "Falha enviar comando: {0}".format(err)
 
-  #self.hi_there = Button(self)
-  #self.hi_there["text"] = "Hello",
-  #self.hi_there["command"] = self.say_hi
-  #self.hi_there.pack({"side": "left"})
+class Application:
 
- def __init__(self, master=None):
-  Frame.__init__(self, master)
-  self.pack()
-  self.createWidgets()
+ def frente(self, widget, data=None):
+  self.button1.connect("clicked", self.conn.fwd, None)
 
-root = Tk()
-app = Application(master=root)
-app.mainloop()
-root.destroy()
+ def tras(self, widget, data=None):
+  self.button2.connect("clicked", self.conn.stop, None)
+
+ def __init__(self):
+  self.conn = sendCmd()
+  self.window1 = gtk.Window(gtk.WINDOW_TOPLEVEL)
+  self.window2 = gtk.Window(gtk.WINDOW_TOPLEVEL)
+  self.button1 = gtk.Button(label="f")
+  self.button2 = gtk.Button(label="s")
+  self.button1.connect("clicked", self.frente, None)
+  self.button2.connect("clicked", self.tras, None)
+  self.window1.add(self.button1)
+  self.window2.add(self.button2)
+  
+  self.button1.show()
+  self.button2.show()
+  self.window1.show()
+  self.window2.show()
+
+
+ def main(self):
+  gtk.main()
+
+if __name__ == "__main__":
+ app = Application()
+ app.main()
